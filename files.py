df0aebfaa050507ff57af6b393fb9559845274b8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-03 14:21:17 +0000

    updates

--- files.py
+++ files.py
@@ -4,6 +4,9 @@
 
 # print(file_data)
 
-f = open('another_file.txt', 'w')
-f.write('Hello World!')
-f.close
+# f = open('another_file.txt', 'w')
+# f.write('Hello World!')
+# f.close
+
+with open('another_file.txt', 'r') as f:
+    file_data = f.read()
