142ae77d533c087c76e1420c075c01df6a4d1fe3
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-03 14:16:51 +0000

    updates

--- files.py
+++ files.py
@@ -1,5 +1,9 @@
-f = open('some_file.txt', 'r')
-file_data = f.read()
-f.close()
+# f = open('some_file.txt', 'r')
+# file_data = f.read()
+# f.close()
 
-print(file_data)
+# print(file_data)
+
+f = open('another_file.txt', 'w')
+f.write('Hello World!')
+f.close
