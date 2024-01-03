36f1ac46de8084eb2993cefff8bd87a1ebaa0019
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-03 12:18:35 +0000

    first commit

---
+++ snakes.py
@@ -0,0 +1,16 @@
+number_of_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Brilliant
+"""
+
+
+print(snake_string * number_of_snakes)
