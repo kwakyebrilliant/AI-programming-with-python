0b0452e4d034036d47a419e60b98de71d5b9265a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-03 13:59:20 +0000

    updates

--- demo.py
+++ demo.py
@@ -3,11 +3,17 @@
 # except:
 #     print('That\s not a valid number')
 
-while True:
-    try:
-        x = int(input('Enter a number: '))
-        break
-    except:
-        print('\nAtempted Input\n')
-    finally:
-        print('\nAtempted Input\n')
+# while True:
+#     try:
+#         x = int(input('Enter a number: '))
+#         break
+#     except:
+#         print('\nAtempted Input\n')
+#     finally:
+#         print('\nAtempted Input\n')
+
+f = open('my_path/my_file.txt', 'r')
+file_data = f.read()
+f.close()
+
+print(file_data)
