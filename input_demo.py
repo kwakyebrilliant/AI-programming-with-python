4696169638fadc324dc23b662e13ee2bc944df01
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-01-03 12:29:21 +0000

    updates

--- input_demo.py
+++ input_demo.py
@@ -1,2 +1,5 @@
-num = int(float(input('Enter a number: ')))
-print("hello" * num)
+# num = int(float(input('Enter a number: ')))
+# print("hello" * num)
+
+result = eval(input("Enter an expression: "))
+print(result)
